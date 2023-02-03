d476810691bca450a396710f9944b1073b71a1a9
Teffy90 <eocampol@unal.edu.co>
2023-02-03 05:31:16 -0500

    Se crea la ruta para que pueda encaminar la solicitud de la API

--- Back/API/routes/api.php
+++ Back/API/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VisitController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function () {
+    echo "prueba";
+});
+
+//Route::get('/visits', '\App\Http\Controllers\VisitController');
+Route::resource('visits', '\App\Http\Controllers\VisitController');
+
